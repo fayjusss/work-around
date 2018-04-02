9287a312e78f6bd3e9741990e255f91edc321045
Fayjus Salehin <fsalehin@live.com>
2018-04-03 01:47:53 +0300

    job provider added

--- src/providers/jobs/jobs.ts
+++ src/providers/jobs/jobs.ts
@@ -1,17 +1,45 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import {
+    AngularFirestore,
+    AngularFirestoreCollection,
+    AngularFirestoreDocument,
+} from 'angularfire2/firestore';
+import * as firebase from 'firebase/app';
+import { Job } from '../../models/job';
+import { DocumentReference } from '@firebase/firestore-types';
 
-/*
-  Generated class for the JobsProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class JobsProvider {
+    userId: string;
+    constructor(
+        public afAuth: AngularFireAuth,
+        public fireStore: AngularFirestore
+    ) {
+        afAuth.authState.subscribe(user => {
+            if (user) {
+                this.userId = user.uid;
+            }
+        });
+    }
+    createJob(
+        title: string,
+        description: string,
+    ): Promise<void> {
+        const jobId: string = this.fireStore.createId();
 
-  constructor(public http: HttpClient) {
-    console.log('Hello JobsProvider Provider');
-  }
-
+        return this.fireStore
+            .doc<Job>(`/jobs/${jobId}`)
+            .set({
+                id: jobId,
+                title,
+                description
+            });
+    }
+    getJobList(): AngularFirestoreCollection<Job> {
+        return this.fireStore.collection<Job>(
+            `/jobs`, // This creates the reference
+            ref => ref.orderBy('title') // This is the query
+        );
+    }
 }
