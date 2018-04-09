aee4117efcced80d5c2ef19676c92a81817acb5b
Fayjus Salehin <fsalehin@live.com>
2018-04-09 10:30:18 +0300

    createBid function created in job provider

--- src/providers/jobs/jobs.ts
+++ src/providers/jobs/jobs.ts
@@ -6,6 +6,7 @@ import {
     AngularFirestoreDocument,
 } from 'angularfire2/firestore';
 import { Job } from '../../models/job';
+import {Bid} from "../../models/bid";
 import { DocumentReference } from '@firebase/firestore-types';
 
 @Injectable()
@@ -21,6 +22,8 @@ export class JobsProvider {
             }
         });
     }
+
+    //Creates a job entry in the jobs collection in firebase
     createJob(
         title: string,
         description: string,
@@ -37,10 +40,34 @@ export class JobsProvider {
                 description
             });
     }
+
     getJobList(): AngularFirestoreCollection<Job> {
         return this.fireStore.collection<Job>(
             `/jobs`, // This creates the reference
             ref => ref.orderBy('title') // This is the query
         );
     }
+    
+    //Creates a bid entry in the bids collection in firebase
+    createBid(
+        jobId: string,
+        time: string,
+        payRequest: string,
+        bidProposal: string,
+    ): Promise<void> {
+        const bidId: string = this.fireStore.createId();
+        const seekerId: string = this.userId;
+
+        return this.fireStore
+            .doc<Bid>(`/jobs/${jobId}`)
+            .set({
+                bidId,
+                jobId,
+                seekerId,
+                time,
+                payRequest,
+                bidProposal
+            });
+    }
+
 }
